80eb13e562d814bac5972b811e2ab2e33197f50c
adexcel171 <godwillokenyi@gmail.com>
2024-01-06 14:52:23 +0100

    initial commit

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,58 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+import fs from "fs"; // Import the 'fs' module to work with file system operations
+
+const router = express.Router();
+
+const uploadDirectory = "uploads/";
+
+// Create the destination directory if it doesn't exist
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDirectory);
+  },
+
+  filename: (req, file, cb) => {
+    const extname = path.extname(file.originalname);
+    cb(null, `${file.originalname}-${Date.now()}${extname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extname = path.extname(file.originalname).toLowerCase();
+  const mimetype = file.mimetype;
+
+  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, (err) => {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    } else if (req.file) {
+      res.status(200).send({
+        message: "Image uploaded successfully",
+        image: `/${req.file.path}`,
+      });
+    } else {
+      res.status(400).send({ message: "No image file provided" });
+    }
+  });
+});
+
+export default router;
