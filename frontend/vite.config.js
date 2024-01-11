4a46d61166b56aa26425f28a3cb3a625770c2a2c
adexcel171 <godwillokenyi@gmail.com>
2024-01-11 10:35:03 +0100

    new render api link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://store-api-4xjx.onrender.com",
+      "/uploads/": "https://store-api-4xjx.onrender.com",
     },
   },
 });
