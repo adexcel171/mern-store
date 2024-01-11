2e64ac08cf4db2e342e9714b9dc6ede74b81eefa
adexcel171 <godwillokenyi@gmail.com>
2024-01-11 15:50:14 +0100

    new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": "https://store-api-4xjx.onrender.com",
-      "/uploads/": "https://store-api-4xjx.onrender.com",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
