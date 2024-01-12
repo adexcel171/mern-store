9e59ccd70c42e6800208edfbb8f9b8244887b03a
adexcel171 <godwillokenyi@gmail.com>
2024-01-12 01:48:34 +0100

    new config

--- backend/index.js
+++ backend/index.js
@@ -35,5 +35,6 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": "https://store-api-4xjx.onrender.com",
-      "/uploads/": "http://localhost:5000",
+      "/uploads/": "https://store-api-4xjx.onrender.com",
     },
   },
 });
