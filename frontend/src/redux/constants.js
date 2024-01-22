a53f632a23aafc1ff3a5548082723ffbf2afc92e
adexcel171 <godwillokenyi@gmail.com>
2024-01-22 06:45:38 +0100

    url for production

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "  http://localhost:5173/";
+export const BASE_URL = "  https://store-faih.onrender.com/";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
